f29e9a10cb2bafc2d7a57dcacc7ce2cfbb28a0e2
Youmin Kim <doumeano0o@gmail.com>
2022-03-09 21:12:07 +0900

    update

---
+++ for문 별찍기.py
@@ -0,0 +1,12 @@
+# for문 별찍기
+#   *  
+#  ***
+# *****
+#*******
+
+for i in range(4):
+    for j in range(3 - i):
+        print(' ', end='')
+    for j in range(2 * i + 1):
+        print('*', end='')
+    print()
