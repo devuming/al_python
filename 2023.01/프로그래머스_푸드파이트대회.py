fa595bb7e3da7c4cd27cf4cdb94d2683b2280762
devum <doumeano0o@gmail.com>
2023-01-21 17:01:59 +0900

    2023.01.23 - 웜엄~ : 프로그래머스

---
+++ 2023.01/프로그래머스_푸드파이트대회.py
@@ -0,0 +1,11 @@
+# 프로그래머스_푸드파이트대회
+
+def solution(food):
+    answer = '0'
+    for i in range(len(food)-1, 0, -1):
+        answer = str(i) * (food[i] // 2) + answer + str(i) * (food[i] // 2)
+        
+    return answer
+
+print(solution([1, 3, 4, 6]))  # "1223330333221"
+print(solution([1, 7, 1, 2]))	# "111303111"
