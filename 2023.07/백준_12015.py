f4c68aef77e537d115399170b70b704eabaf5ad9
devum <doumeano0o@gmail.com>
2023-07-05 23:12:48 +0900

    2023.07.05 - LIS 이진탐색

---
+++ 2023.07/백준_12015.py
@@ -0,0 +1,12 @@
+import sys
+import bisect
+n = int(sys.stdin.readline().rstrip())
+a = list(map(int, sys.stdin.readline().rstrip().split()))
+lis = [a[0]]
+for i in range(1, n):
+    if lis[-1] < a[i]:
+        lis.append(a[i])
+    else:
+        idx = bisect.bisect_left(lis, a[i])
+        lis[idx] = a[i]
+print(len(lis))
